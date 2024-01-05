7a7baaf5b06ca98c0c7012383e0eb1cb925e2fa8
Rachel J <rcj.rachel@gmail.com>
2024-01-04 23:24:30 -0600

    Ready for testing

--- SontiCast/SontiCast/settings.py
+++ SontiCast/SontiCast/settings.py
@@ -128,13 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "https://sonicast-beta.onrender.com/static"
 STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
